d419a2d4756b58aa0b2c07c88a4d737c4bf95702
Williest <williestnyainaandry@gmail.com>
2025-05-01 17:05:16 +0300

    feat: add championship_management.sql

--- db/migration/championship_management.sql
+++ db/migration/championship_management.sql
@@ -20,12 +20,45 @@ CREATE TABLE league_season(
     league_id UUID REFERENCES league(league_id)
 );
 
--- CREATE TABLE match(
---     match_id UUID PRIMARY KEY,
---     league_id UUID REFERENCES league(league_id),
---     match_date TIMESTAMP NOT NULL,
---     home_team VARCHAR NOT NULL,
---     away_team VARCHAR NOT NULL
--- );
+CREATE TABLE coach(
+    coach_id UUID PRIMARY KEY,
+    coach_name VARCHAR NOT NULL,
+    coach_nationality VARCHAR NOT NULL
+);
 
--- CREATE TABLE
+CREATE TABLE ranking(
+    ranking_id UUID PRIMARY KEY,
+    league_id UUID REFERENCES league(league_id),
+    rank INT UNIQUE NOT NULL,
+    points INT NOT NULL
+);
+
+CREATE TABLE club(
+    club_id UUID PRIMARY KEY,
+    league_id UUID REFERENCES league(league_id),
+    coach_id UUID REFERENCES coach(coach_id),
+    ranking_id UUID REFERENCES ranking(ranking_id),
+    club_name VARCHAR UNIQUE NOT NULL,
+    creation_year DATE NOT NULL,
+    acronym VARCHAR(3) NOT NULL,
+    stadium_name VARCHAR NOT NULL
+);
+
+CREATE TABLE match(
+    match_id UUID PRIMARY KEY,
+    league_id UUID REFERENCES league(league_id),
+    match_date TIMESTAMP NOT NULL,
+    home_team UUID REFERENCES club(club_id),
+    away_team UUID REFERENCES club(club_id)
+);
+
+CREATE TYPE player_position_in_field AS ENUM ('FORWARD', 'MIDFIELDER', 'DEFENDER', 'GOALKEEPER');
+CREATE TABLE player(
+    player_id UUID PRIMARY KEY,
+    club_id UUID REFERENCES club(club_id),
+    player_name VARCHAR NOT NULL,
+    player_number INT NOT NULL,
+    player_nationality VARCHAR NOT NULL,
+    player_birthday DATE NOT NULL,
+    player_position player_position_in_field NOT NULL
+);
