d76fca8bad2a2591f349ffbb8a456d0cd1639e39
Williest <williestnyainaandry@gmail.com>
2025-05-01 08:54:23 +0300

    chore(doc): add openAPI specification

---
+++ src/main/java/com/williest/onechampionshipapi/OneChampionshipApiApplication.java
@@ -0,0 +1,13 @@
+package com.williest.onechampionshipapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneChampionshipApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneChampionshipApiApplication.class, args);
+	}
+
+}
